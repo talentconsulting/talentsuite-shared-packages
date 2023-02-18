54e8174917d1b6204979b229fe0428519e5a26b5
Alasdair Reid <ali.reid@live.co.uk>
2023-02-18 09:49:54 +0000

    Added talent suite configuration helper

---
+++ talentsuite-configuration/TalentConsulting.TalentSuite.Configuration/Helpers/EnvironmentVariableNames.cs
@@ -0,0 +1,8 @@
+namespace TalentConsulting.TalentSuite.Configuration
+{
+    public static class EnvironmentVariableNames
+    {
+        public const string EnvironmentName = "APPSETTING_EnvironmentName";
+        public const string ConfigurationStorageConnectionString = "APPSETTING_ConfigurationStorageConnectionString";
+    }
+}
