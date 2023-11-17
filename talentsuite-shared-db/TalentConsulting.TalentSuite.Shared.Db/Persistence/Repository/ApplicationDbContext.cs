792825d2d83fe35aa0f4656720f52a2339d10c8f
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-11-17 15:12:15 +0000

    Removed unecessay usings

--- talentsuite-shared-db/TalentConsulting.TalentSuite.Shared.Db/Persistence/Repository/ApplicationDbContext.cs
+++ talentsuite-shared-db/TalentConsulting.TalentSuite.Shared.Db/Persistence/Repository/ApplicationDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using TalentConsulting.TalentSuite.Shared.Db.Entities;
 using TalentConsulting.TalentSuite.Shared.Db.Interceptors;
 
